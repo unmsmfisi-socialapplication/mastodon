b12c260154088c05b16225ed0bf93a1c092d06fe
Edson Ticona Zegarra <eticonazegarra@gmail.com>
2024-11-22 20:39:09 +0100

    return 422 status code when media file is not supported

--- app/controllers/api/v2/media_controller.rb
+++ app/controllers/api/v2/media_controller.rb
@@ -2,7 +2,13 @@
 
 class Api::V2::MediaController < Api::V1::MediaController
   def create
+    unless supported_mime_type?(params["file"].content_type)
+      render json: file_type_error, status: 415
+      return
+    end
+
     @media_attachment = current_account.media_attachments.create!(media_and_delay_params)
+
     render json: @media_attachment, serializer: REST::MediaAttachmentSerializer, status: status_from_media_processing
   rescue Paperclip::Errors::NotIdentifiedByImageMagickError
     render json: file_type_error, status: 422
@@ -19,5 +25,11 @@ class Api::V2::MediaController < Api::V1::MediaController
 
   def status_from_media_processing
     @media_attachment.not_processed? ? 202 : 200
+  end
+
+  def supported_mime_type?(content_type)
+    MediaAttachment::VIDEO_MIME_TYPES.include?(content_type) ||
+      MediaAttachment::AUDIO_MIME_TYPES.include?(content_type) ||
+      MediaAttachment::IMAGE_MIME_TYPES.include?(content_type)
   end
 end
