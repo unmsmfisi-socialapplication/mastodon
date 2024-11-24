2c0dc038d3879e27b91c2caa83e6da94a999bfc2
Nicole Morales <moralesrobladillon@gmail.com>
2024-11-24 12:00:45 -0500

    Add appeals and strikes controller

---
+++ app/controllers/admin/disputes/appeal_notes_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Admin::Disputes::AppealNotesController < Admin::BaseController
+  before_action :set_appeal_note, only: [:destroy]
+
+  def create
+    authorize :report_note, :create?
+
+    @appeal_note = current_account.appeal_notes.new(resource_params)
+    @appeal      = @appeal_note.appeal
+
+    if @appeal_note.save
+      redirect_to after_create_redirect_path, notice: I18n.t('admin.disputes.appeal_notes.created_msg')
+    else
+      @appeal_notes = @appeal.notes.chronological.includes(:account)
+      @form         = Admin::StatusBatchAction.new
+      
+      render 'admin/disputes/strikes/show'
+    end
+  end
+
+  def destroy
+    authorize @appeal_note, :destroy?
+    @appeal_note.destroy!
+    redirect_to admin_disputes_strike_path(@appeal_note.appeal_id), notice: I18n.t('admin.disputes.appeal_notes.destroyed_msg')
+  end
+
+  private
+
+  def after_create_redirect_path
+    if params[:create_and_resolve]
+      admin_disputes_appeals_path
+    else
+      admin_disputes_strike_path(@appeal.strike)
+    end
+  end
+
+  def resource_params
+    params.require(:appeal_note).permit(
+      :content,
+      :appeal_id
+    )
+  end
+
+  def set_appeal_note
+    @appeal_note = AppealNote.find(params[:id])
+  end
+end
---
+++ app/controllers/admin/disputes/strikes_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Admin::Disputes::StrikesController <  Admin::BaseController
+  before_action :set_strike, only: [:show]
+
+  def index
+    @strikes = current_account.strikes.latest
+  end
+
+  def show
+    authorize @strike, :show?
+    @appeal = @strike.appeal
+    @appeal_note = @appeal.notes.new
+    @appeal_notes =  @appeal.notes.chronological.includes(:account)
+
+  end
+
+  private
+
+  def set_strike
+    @strike = AccountWarning.find(params[:id])
+  end
+end
