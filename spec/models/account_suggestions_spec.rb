7a154daa239246b73be96be9ba629d97991729d3
AdrianIsmaelQuispeFajardo <adrianismael.quispefajardo@gmail.com>
2024-12-06 18:46:10 +0000

    Add core functionality specs

--- spec/models/account_suggestions_spec.rb
+++ spec/models/account_suggestions_spec.rb
@@ -34,4 +34,68 @@ RSpec.describe AccountSuggestions do
       expect(suggestion_service.instance_variable_get(:@account)).to eq(account)
     end
   end
+
+  describe '#remove' do
+    let(:target_account_id) { 123 }
+
+    it 'creates a FollowRecommendationMute record for the specified account' do
+      mute = instance_spy(FollowRecommendationMute)
+      allow(FollowRecommendationMute).to receive(:create).and_return(mute)
+
+      suggestion_service.remove(target_account_id)
+
+      expect(FollowRecommendationMute).to have_received(:create).with(
+        account_id: account.id,
+        target_account_id: target_account_id
+      )
+    end
+  end
+
+  describe '#get' do
+    let(:limit) { 10 }
+    let(:offset) { 0 }
+    let(:mock_suggestions) do
+      [
+        [1, ['friends_of_friends']],
+        [2, ['similar_profiles']],
+        [3, ['global']],
+      ]
+    end
+
+    let(:suggested_user_one) { Fabricate(:account, id: 1, username: 'user1') }
+    let(:suggested_user_two) { Fabricate(:account, id: 2, username: 'user2') }
+    let(:suggested_user_three) { Fabricate(:account, id: 3, username: 'user3') }
+    let(:suggested_accounts) { [suggested_user_one, suggested_user_two, suggested_user_three] }
+
+    before do
+      allow(Rails.cache).to receive(:fetch).and_return(mock_suggestions)
+      relation_double = instance_double(ActiveRecord::Relation, includes: suggested_accounts)
+      allow(Account).to receive(:where).and_return(relation_double)
+      allow(suggested_accounts).to receive(:index_by).and_return(suggested_accounts.index_by(&:id))
+    end
+
+    context 'when fetching basic suggestions' do
+      it 'returns the correct number of suggestions' do
+        results = suggestion_service.get(limit)
+        expect(results.length).to eq(3)
+      end
+
+      it 'returns suggestion objects with the expected structure' do
+        results = suggestion_service.get(limit)
+        expect(results).to all(be_a(AccountSuggestions::Suggestion))
+      end
+    end
+
+    context 'when applying pagination parameters' do
+      it 'respects the limit parameter' do
+        results = suggestion_service.get(2)
+        expect(results.length).to eq(2)
+      end
+
+      it 'respects the offset parameter' do
+        results = suggestion_service.get(limit, 1)
+        expect(results.length).to eq(2)
+      end
+    end
+  end
 end
